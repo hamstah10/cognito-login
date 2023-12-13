a74b20a25feaca9ca9c80ebae50b11121ee17a74
hamstah100 <andre.sancken@polarise.ai>
2023-12-13 23:18:20 +0100

    Login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class LoginController extends AbstractController
 {
@@ -27,5 +28,14 @@ class LoginController extends AbstractController
     {
         // controller can be blank: it will never be called!
         throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+    #[Route('/portal/dashboard', name: 'after_login_redirect')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('portal/dashboard.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,11 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
-
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]',
+        pattern: /\.(png|jpg|jpeg|gif|ico)$/
+    })
     /*
      * ENTRY CONFIG
      *
@@ -52,6 +56,8 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = '3.23';
     })
+
+    
 
     // enables Sass/SCSS support
     .enableSassLoader()
